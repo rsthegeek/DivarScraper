ff3fccd252159695014abe116dccac209d79d6c6
MohammadRasoul Fekri <rasoolsystem@me.com>
2020-04-03 01:42:48 +0430

    fix

--- main.php
+++ main.php
@@ -2,10 +2,11 @@
 
 define('START', microtime(true));
 
-require __DIR__.'/Util.php';
-require __DIR__.'/Telegram.php';
-require __DIR__.'/DivarCrawler.php';
+require __DIR__ . '/Util.php';
+require __DIR__ . '/Telegram.php';
+require __DIR__ . '/DivarCrawler.php';
 
 $master = new App\DivarCrawler();
-$master->crawl();
+//$master->crawl();
 
+echo "\n\n\033[42mFinished in: " . (microtime(true) - START) . " seconds\033[0m\n";
