72cf5af4a713fcfe454eee98c247a3c456f8de1e
MohammadRasoul Fekri <rasoolsystem@me.com>
2020-05-17 16:23:35 +0430

    added endsWith util

--- Util.php
+++ Util.php
@@ -68,4 +68,13 @@ class Util
     {
         return __DIR__ . '/' . $fileName;
     }
+
+    public static function endsWith($string, $endString)
+    {
+        $len = strlen($endString);
+        if ($len == 0) {
+            return true;
+        }
+        return (substr($string, -$len) === $endString);
+    }
 }
